8599b647cbd60c7ea3e97465b2c4f5e956e5aa54
rostom44 <blackgamerdv7@gmail.com>
2024-06-05 23:27:00 +0200

    minor addition

--- src/components/home/intro.jsx
+++ src/components/home/intro.jsx
@@ -1,3 +1,4 @@
+import { MdOutlineDoubleArrow } from "react-icons/md";
 import "./home.css";
 import { useTranslation } from "react-i18next";
 export default function Intro() {
@@ -10,7 +11,12 @@ export default function Intro() {
         <span>ROSTOM</span>
       </h1>
       <h2>{t("common.translate-fullStack")}</h2>
-      <p></p>
+      <p className="more">
+        {t("common.translate-aboutMe")}
+        <a href="/about">
+          <MdOutlineDoubleArrow />
+        </a>
+      </p>
     </div>
   );
 }
