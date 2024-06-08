2b3877f74ac6b3d1af126db570516c3c9d05c993
rostom44 <blackgamerdv7@gmail.com>
2024-06-08 19:28:34 +0200

    added react logo

--- src/components/home/intro.jsx
+++ src/components/home/intro.jsx
@@ -1,15 +1,28 @@
+/* eslint-disable react/no-unknown-property */
 import { MdOutlineDoubleArrow } from "react-icons/md";
-import "./home.css";
-import { useTheme } from "../../darkTheme/ThemeProvider";
 
+import { useTheme } from "../../darkTheme/ThemeProvider";
 import { useTranslation } from "react-i18next";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import ReactLogo from "./ReactLogo";
+
+import "./home.css";
 export default function Intro() {
   const { t } = useTranslation();
   const { darkMode } = useTheme();
   return (
     <div className="box-container">
-      <div className={darkMode ? "img-circle dark" : "img-circle"}>
-        <img src="src\assets\anime-dance.gif" alt="pfp" />
+      <div className="introHeader">
+        <div className={darkMode ? "img-circle dark" : "img-circle"}>
+          <img src="src\assets\anime-dance.gif" alt="pfp" />
+        </div>
+        <Canvas className="reactLogo">
+          <ambientLight />
+          <OrbitControls autoRotate enableZoom={false} />
+          <ReactLogo />
+          <Environment preset="forest" />
+        </Canvas>
       </div>
       <h1>
         {t("common.translate-hi")}
