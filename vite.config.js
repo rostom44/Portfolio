927296835c3e3b88eed1f8136ff96be4bd3cf03d
rostom44 <blackgamerdv7@gmail.com>
2024-10-08 22:07:38 +0200

    wasting time

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+// Define the base URL based on the mode
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: import.meta.env.MODE === "production" ? "/Portfolio/" : "/", // Use '/Portfolio/' in production
-});
+  base: mode === "production" ? "/Portfolio/" : "/", // Use 'mode' argument
+}));
